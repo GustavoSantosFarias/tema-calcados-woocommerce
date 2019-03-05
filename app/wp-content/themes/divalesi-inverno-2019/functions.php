5e1e20c0faa158886217002863c99086eb67d896
fariasgustavo <devgustavofarias@gmail.com>
2019-03-05 17:49:29 -0300

    Move actions home from /inc/functions.php to ./functions.php

--- app/wp-content/themes/divalesi-inverno-2019/functions.php
+++ app/wp-content/themes/divalesi-inverno-2019/functions.php
@@ -9,3 +9,26 @@ function load_setup_theme(){
     require __DIR__."/inc/setup.php";
 }
 add_action("init","load_setup_theme");
+
+
+function products_featureds(){
+    get_template_part("inc/templates/products_featureds_view");
+}
+add_action("content_products_featureds","products_featureds");
+
+function categories_home(){
+    get_template_part("inc/templates/categories_home_view");
+}
+add_action("content_categories_home","categories_home");
+
+function banners_home(){
+    get_template_part("inc/templates/banners_home_view");
+}
+add_action("content_banners_home","banners_home");
+
+function home(){
+    do_action("content_banners_home");
+    do_action("content_products_featureds");
+    do_action("content_categories_home");
+}
+add_action("content_home","home");
--- app/wp-content/themes/divalesi-inverno-2019/inc/functions.php
+++ app/wp-content/themes/divalesi-inverno-2019/inc/functions.php
@@ -1,23 +1,2 @@
 <?php
 
-function products_featureds(){
-    get_template_part("templates/products_featureds_view.php");
-}
-add_action("content_products_featureds","products_featureds");
-
-function categories_home(){
-    get_template_part("templates/categories_home_view.php");
-}
-add_action("content_categories_home","categories_home");
-
-function banners_home(){
-    get_template_part("templates/banners_home_view.php");
-}
-add_action("content_banners_home","banners_home");
-
-function home(){
-    do_action("content_banners_home");
-    do_action("content_products_featureds");
-    do_action("content_categories_home");
-}
-add_action("content_home","home");
