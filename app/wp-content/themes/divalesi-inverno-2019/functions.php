d6bf5858b41b824c3b68794fc1b405ef566ec302
fariasgustavo <devgustavofarias@gmail.com>
2019-03-05 14:58:40 -0300

    Setup home content actions

--- app/wp-content/themes/divalesi-inverno-2019/functions.php
+++ app/wp-content/themes/divalesi-inverno-2019/functions.php
@@ -4,3 +4,8 @@ function mytheme_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+function load_setup_theme(){
+    require __DIR__."/inc/setup.php";
+}
+add_action("init","load_setup_theme");
---
+++ app/wp-content/themes/divalesi-inverno-2019/home.php
@@ -0,0 +1,8 @@
+<?php 
+/* Template Name: Home */ 
+
+get_header();
+
+do_action("content_home");
+
+get_footer(); 
