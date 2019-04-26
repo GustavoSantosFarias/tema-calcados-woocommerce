b2d7faa677e079c2104abc8a474cd9328a753c5f
fariasgustavo <santosgustavo60@gmail.com>
2019-04-26 13:44:39 -0300

    Destop footer style completed

--- app/wp-content/themes/divalesi-inverno-2019/footer.php
+++ app/wp-content/themes/divalesi-inverno-2019/footer.php
@@ -18,39 +18,42 @@
             </div>
 
             <div class="footer-menus">
-                <div>
-                    <h4>ATENDIMENTO</h4>
-                    <?php divalesi_menu("footer-atendimento"); ?>
-                </div>
+                <div class="row">
+                    <div class="col-12 col-md-3">
+                        <h4>ATENDIMENTO</h4>
+                        <?php divalesi_menu("footer-atendimento"); ?>
+                    </div>
 
-                <div>
-                    <h4>LINKS ÚTEIS</h4>
-                    <?php divalesi_menu("links-uteis-footer"); ?>
-                </div>
-                
-                <div>
-                    <h4>NOSSAS REDES</h4>
-                    <?php divalesi_menu("nossas-redes"); ?>
-                </div>
+                    <div class="col-12 col-md-3">
+                        <h4>LINKS ÚTEIS</h4>
+                        <?php divalesi_menu("links-uteis-footer"); ?>
+                    </div>
+                    
+                    <div class="col-12 col-md-3">
+                        <h4>NOSSAS REDES</h4>
+                        <?php divalesi_menu("nossas-redes"); ?>
+                    </div>
 
-                <div>
-                    <h4>SOBRE A DIVALESI</h4>
-                    <p class="secondary-title">
-                        Transformamos nossa paixão por moda em calçados exclusivos,
-                        que acompanham com estilo e personalidade, o dia a dia da mulher brasileira.
-                        Confira nossas novidades, fique por dentro do andamento dos seus pedidos e venda ainda mais, através do nosso aplicativo, disponível gratuitamente para Android e IOS.
-                    </p>
-                </div>
+                    <div class="col-12 col-md-3">
+                        <h4>SOBRE A DIVALESI</h4>
+                        <p class="secondary-title">
+                            Transformamos nossa paixão por moda em calçados exclusivos,
+                            que acompanham com estilo e personalidade, o dia a dia da mulher brasileira.
+                            Confira nossas novidades, fique por dentro do andamento dos seus pedidos e venda ainda mais, através do nosso aplicativo, disponível gratuitamente para Android e IOS.
+                        </p>
 
-                <address>
-                    <p class="secondary-title"><i class="fas fa-paper-plane"></i>Rua 7 de Setembro – 1203, Três Coroas / RS</p>
-                    <p class="secondary-title"><i class="fas fa-mobile"></i>Fone: (51) 3546-6777</p>
-                    <p class="secondary-title"><i class="fab fa-whatsapp"></i>(51) 9 9161-3239</p>
-                </address>
+                        <address>
+                            <p class="secondary-title"><i class="fas fa-paper-plane"></i>Rua 7 de Setembro – 1203, Três Coroas / RS</p>
+                            <p class="secondary-title"><i class="fas fa-mobile"></i>Fone: (51) 3546-6777</p>
+                            <p class="secondary-title"><i class="fab fa-whatsapp"></i>(51) 9 9161-3239</p>
+                        </address>
 
-                <div class="text-center">
-                    <img class="img-fluid" src="<?= THEME_ASSETS_URI . 'images/payments-available-methods.png' ?>" alt="Métodos de pagamentos disponíveis">
-                </div>
+                        <div class="text-center">
+                            <img class="img-fluid" src="<?= THEME_ASSETS_URI . 'images/payments-available-methods.png' ?>" alt="Métodos de pagamentos disponíveis">
+                        </div>
+
+                    </div>
+                </div>   
             </div>
             
         </div>   
