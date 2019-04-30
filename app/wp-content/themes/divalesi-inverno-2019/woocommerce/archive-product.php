d289fe5c0cebeb333486bd93b400d31a85c9d65c
fariasgustavo <santosgustavo60@gmail.com>
2019-04-30 17:40:02 -0300

    Starting development a select ordering in shop page

--- app/wp-content/themes/divalesi-inverno-2019/woocommerce/archive-product.php
+++ app/wp-content/themes/divalesi-inverno-2019/woocommerce/archive-product.php
@@ -37,60 +37,74 @@ get_header();
 
 </header>
 
-<div class="container">
+<div class="container clearfix">
+	
+	<?php woocommerce_output_all_notices() ?>
+
 	<div class="clearfix">
 		<div class="float-right">
-<?php
+			<div class="order-filter clearfix">
+				<?php
 
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
-do_action( 'woocommerce_before_main_content' );
+				/**
+				 * Hook: woocommerce_before_main_content.
+				 *
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 * @hooked WC_Structured_Data::generate_website_data() - 30
+				 */
+				woocommerce_breadcrumb();
+				woocommerce_catalog_ordering();
 
-if ( woocommerce_product_loop() ) {
+				?>
 
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
+				<div class="laber-filer-mobile d-none-sm d-none-md d-none-lg">
+					<span>Filtrar</span>
+					<i class="fas fa-filter"></i>
+				</div>
+			</div>
 
-	woocommerce_product_loop_start();
+			<?php
+			if ( woocommerce_product_loop() ) {
 
-	do_action("divalesi_shop_loop");
+				/**
+				 * Hook: woocommerce_before_shop_loop.
+				 *
+				 * @hooked woocommerce_output_all_notices - 10
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				//do_action( 'woocommerce_before_shop_loop' );
 
-	woocommerce_product_loop_end();
+				woocommerce_product_loop_start();
 
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
+				do_action("divalesi_shop_loop");
 
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action( 'woocommerce_after_main_content' );
+				woocommerce_product_loop_end();
 
-?>
+				/**
+				 * Hook: woocommerce_after_shop_loop.
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			} else {
+				/**
+				 * Hook: woocommerce_no_products_found.
+				 *
+				 * @hooked wc_no_products_found - 10
+				 */
+				do_action( 'woocommerce_no_products_found' );
+			}
+
+			/**
+			 * Hook: woocommerce_after_main_content.
+			 *
+			 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+			 */
+			do_action( 'woocommerce_after_main_content' );
+
+			?>
 
 		</div>	
 
@@ -103,7 +117,7 @@ do_action( 'woocommerce_after_main_content' );
 		</div>	
 	</div>	
 </div>
-<?php
 
+<?php
 get_footer();
 
