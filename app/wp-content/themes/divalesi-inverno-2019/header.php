6f13fc6748e7fae4f9b0398516cb559a6c4e3439
fariasgustavo <santosgustavo60@gmail.com>
2019-05-02 10:44:28 -0300

    Add Owl Carousel plugin

--- app/wp-content/themes/divalesi-inverno-2019/header.php
+++ app/wp-content/themes/divalesi-inverno-2019/header.php
@@ -6,9 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo the_title()?></title>
     <link rel="stylesheet" href="<?= THEME_ASSETS_URI . 'css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="<?= THEME_ASSETS_URI . 'css/owl.carousel.min.css' ?>">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= assetsVersion(THEME_ASSETS_DIR."css/main.min.css") ?>">
     <script src="<?= THEME_ASSETS_URI . "js/plugins/jquery.min.js" ?>"></script>
+    <script src="<?= THEME_ASSETS_URI . "js/plugins/owl.carousel.min.js" ?>"></script>
 </head>
 <body <?php body_class("hind")?>>
     <header>
